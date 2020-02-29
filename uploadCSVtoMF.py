bd333c34090d8b4c5ac7594a25d724c586afa8e6
hsgwa <hasegawa.ats@gmail.com>
2020-02-29 16:01:53 +0900

    clean: divide selenium transaction as WebController

--- uploadCSVtoMF.py
+++ uploadCSVtoMF.py
@@ -1,124 +1,140 @@
 # coding: UTF-8
-import sys
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-import pandas as pd
-import chromedriver_binary
+
+class WebController():
+    import chromedriver_binary
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+    def __init__(self, username, password):
+        self.__username = username
+        self.__password = password
+        self.__driver = webdriver.Chrome()
+        self.__driver.implicitly_wait(10)
+
+    def __del__(self):
+        self.__driver.quit()
+
+    def login(self):
+        self.__driver.get("https://moneyforward.com/users/sign_in")
+        elem = self.__driver.find_element_by_id(
+            "sign_in_session_service_email")
+        elem.clear()
+        elem.send_keys(self.__username)
+        elem = self.__driver.find_element_by_id(
+            "sign_in_session_service_password")
+        elem.clear()
+        elem.send_keys(self.__password)
+        elem = self.__driver.find_element_by_id("login-btn-sumit")
+        elem.click()
+
+    def open_payment(self):
+        self.__driver.get("https://moneyforward.com/cf#cf_new")
+        element = WebDriverWait(self.__driver, 10).until(
+            EC.presence_of_element_located((By.ID, "submit-button")))
+
+    def open_next_payment(self):
+        element = WebDriverWait(self.__driver, 10).until(
+            EC.presence_of_element_located((By.ID, "confirmation-button")))
+        elem = self.__driver.find_element_by_id("confirmation-button")
+        elem.click()
+
+    def open_plus_payment(self):
+        elem = self.__driver.find_element_by_class_name("plus-payment").click()
+
+    def fill_content(self, content):
+        elem = self.__driver.find_element_by_id("js-content-field")
+        elem.clear()
+        elem.send_keys(content)
+
+    def fill_payment(self, price):
+        if price == 0:
+            raise ValueError("paiment is zero.")
+
+        if price > 0:
+            elem = self.__driver.find_element_by_class_name(
+                "plus-payment").click()
+        else:
+            elem = self.__driver.find_element_by_class_name(
+                "minus-payment").click()
+
+        elem = self.__driver.find_element_by_id("appendedPrependedInput")
+        elem.clear()
+        elem.send_keys(abs(price))
+
+    def save_payment(self):
+        elem = self.__driver.find_element_by_id("submit-button").click()
+
+    def fill_date(self, date):
+        elem = self.__driver.find_element_by_id("updated-at")
+        elem.clear()
+        elem.send_keys(date)
+
+    def select_large_category(self, category):
+        elem = self.__driver.find_element_by_id(
+            "js-large-category-selected").click()
+        elem = self.__driver.find_element_by_xpath("//a[text()='" + category +
+                                                   "' and @class='l_c_name']")
+        elem.click()
+
+    def select_middle_category(self, category):
+        elem = self.__driver.find_element_by_id(
+            "js-middle-category-selected").click()
+        elem = self.__driver.find_element_by_xpath("//a[text()='" + category +
+                                                   "' and @class='m_c_name']")
+        elem.click()
+
 
 def doUpload(input_file):
-    from dotenv import load_dotenv
     import os
+    from time import sleep
+    import pandas as pd
+    from dotenv import load_dotenv
     load_dotenv()
 
-    surl = "https://moneyforward.com/users/sign_in"
-    iurl = "https://moneyforward.com/cf#cf_new"
     user = os.environ.get("USERNAME")
     password = os.environ.get("PASSWORD")
+
+    print("USERNAME")
     print(user, password)
 
     if user == None:
-      print('USERNAME is not set. Please create .env file to set login info.')
-      return
+        print('USERNAME is not set. Please create .env file to set login info.')
+        return
+    else:
+        print("USERNAME : {}".format(user))
 
     if password == None:
-      print('PASSWORD is not set. Please create .env file to set login info.')
-      return
+        print('PASSWORD is not set. Please create .env file to set login info.')
+        return
+    else:
+        print("PASSWORD : {}".format("*" * len(password)))
 
-    try:
-        driver = webdriver.Chrome()
-        driver.implicitly_wait(10)
-        driver.get(surl)
+    controller = WebController(user, password)
+    df = pd.read_csv(input_file)
 
-        # login
-        elem = driver.find_element_by_id("sign_in_session_service_email")
-        elem.clear()
-        elem.send_keys(user)
-        elem = driver.find_element_by_id("sign_in_session_service_password")
-        elem.clear()
-        elem.send_keys(password)
-        elem = driver.find_element_by_id("login-btn-sumit")
-        elem.click()
+    controller.login()
+    controller.open_payment()
 
-        # open
-        driver.get(iurl)
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "submit-button")))
+    for count, row in df.iterrows():
+        controller.fill_date(row['日付'])
+        controller.fill_payment(int(row['金額']))
+        controller.select_large_category(row['大分類'])
+        controller.select_middle_category(row['中分類'])
+        controller.fill_content(row['備考'])
+        sleep(2)
+        controller.save_payment()
 
-        df = pd.read_csv(input_file)
-        for count, row in df.iterrows():
-            if int(row['金額']) > 0:
-                # case of income
-                print("[" + str(count) + "] " + "case of income : ")
-                price = int(row['金額'])
-
-                elem = driver.find_element_by_class_name(
-                    "plus-payment").click()
-
-            elif int(row['金額']) < 0:
-                # case of outgo
-                print("[" + str(count) + "] " + "case of outgo : ")
-                price = int(row['金額'])
-
-            else:
-                print("[" + str(count) + "] " + "Error format : ")
-
-            #input price info
-            elem = driver.find_element_by_id("appendedPrependedInput")
-            elem.clear()
-            elem.send_keys(abs(int(row['金額'])))
-
-            #input large-category info
-            elem = driver.find_element_by_id(
-                "js-large-category-selected").click()
-            sleep(1)
-            elem = driver.find_element_by_xpath(
-                "//a[text()='" + row['大分類'] +
-                "' and @class='l_c_name']").click()
-            sleep(1)
-            #input middle-category info
-            elem = driver.find_element_by_id(
-                "js-middle-category-selected").click()
-            sleep(1)
-            elem = driver.find_element_by_xpath(
-                "//a[text()='" + row['中分類'] +
-                "' and @class='m_c_name']").click()
-            sleep(1)
-            #input content-field info
-            elem = driver.find_element_by_id("js-content-field")
-            elem.clear()
-            elem.send_keys(row['備考'])
-
-            #input date info
-            elem = driver.find_element_by_id("updated-at")
-            elem.clear()
-            elem.send_keys(row['日付'])
-
-            #save
-            elem = driver.find_element_by_id("submit-button").click()
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.ID, "confirmation-button")))
-            elem = driver.find_element_by_id("confirmation-button").click()
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "submit-button")))
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.CLASS_NAME, "plus-payment")))
-
-        print("End procedure of " + input_file)
-        driver.quit()
-
-    except ValueError:
-        print("Oops! Some Error are occured.")
+        if count < len(df):
+            controller.open_next_payment()
 
     return 1
 
 
 if __name__ == '__main__':
+    import sys
     if len(sys.argv) != 2:
         print("No input_file!")
         print("usage: python uploadCSVtoMF.py input_data.csv")
